0c039b559623c504fe2ce51bdff2a19d391e042a
Yash Krishan <yashkmkrishan@gmail.com>
2018-07-04 20:29:04 +0530

    Update text.py

--- text.py
+++ text.py
@@ -1,8 +1,13 @@
 import json,paralleldots,csv
 
+#file name from facebook downloaded files
 filename = "your_posts.json"
+
+#loading the files of json component
 with open(filename,'r') as j:
 	data = json.load(j)
+	
+#extracting the post data from the raw data ibtained
 a = []
 for i in data.get('status_updates'):
 	if i.get('data') is not None:
@@ -11,16 +16,21 @@ b = []
 for i in a:
 	b.append(i[0].get('post'))
 
+#authorising api key
 paralleldots.set_api_key("2eJksCHNsoqj7uZYHmEcgG9P3hiF7A9nHb4tmwvUmH8")
 
 di = {}
 new = {}
+
+#list of all the methods
 li = [paralleldots.sentiment,paralleldots.emotion, paralleldots.abuse,paralleldots.ner,paralleldots.keywords,paralleldots.taxonomy,paralleldots.phrase_extractor]
 for j in li:
-	for i in b[0:51]:
+#evaluating each functional apis
+	for i in b[0:51]:#evaluating for list of first 50 values
 		di = j(i)
 		new[i] = di
 	for i in new:
+		#writing the values in csv files
 		with open('{}.csv'.format(("{}".format(j)).split(" ")[1][4:]), 'a') as csvfile:
 			writer = csv.writer(csvfile, delimiter = ',')
 			writer.writerow([i,new.get(i)])
